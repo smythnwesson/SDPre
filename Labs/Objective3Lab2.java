ff498c710122f4014f99df502a8c6cad6ed3b973
Mathew Smyth <smythnwesson@gmail.com>
2021-07-17 11:56:28 -0700

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+  public static void main(String[] args)  {
+    double num = Math.sqrt(3 * 3);
+    double result = Math.sqrt(4) + Math.sqrt(9);
+    double side1 = 10.0;
+    double side2 = 8.0;
+    double hypotenuse = Math.sqrt ((side1 * side1) + (side2 * side2));
+    System.out.println(hypotenuse);
+  }
+}
