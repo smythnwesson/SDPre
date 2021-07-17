5125b4882699ef520004eaed94ea6a6d0eed56ac
Mathew Smyth <smythnwesson@gmail.com>
2021-07-17 09:57:49 -0700

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args)  {
+    String name = "charles Peterson";
+    int age = 47;
+    String state= "Arkansas";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
