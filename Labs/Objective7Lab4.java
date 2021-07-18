7bfd828528efc09c8ba5fd65632a5be4fd2ce18c
Mathew Smyth <smythnwesson@gmail.com>
2021-07-18 14:58:52 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 20;
+    int sum = 0;
+    int add = 1;
+     while(add <= counter){
+       sum = sum + add;
+       add++;
+       }
+       System.out.println(sum);
+  }
+}
