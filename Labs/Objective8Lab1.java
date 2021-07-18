aad9b780af29422cd4f6b0d13d56fb56034bd797
Mathew Smyth <smythnwesson@gmail.com>
2021-07-18 16:11:22 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int i = 1;
+    for (i = 1; i <= 10; i++){
+      System.out.println(i);
+    }
+    for (i = 10; i >= 1; i--){
+      System.out.println(i);
+    }
+
+  }
+}
